b813731ab99087e75c828aaad349d515d196b1a9
Gregory <z5480004@ad.unsw.edu.au>
2026-04-24 14:23:38 +1000

    Feat: Added Functionality of loading JSON file to create parameters for CLI

--- tests/test_hypertree_signatures.py
+++ tests/test_hypertree_signatures.py
@@ -27,5 +27,20 @@ class Extension4Tests(unittest.TestCase):
         
         self.assertTrue(self.system_controller.verify_message(msg2, sig1))
 
+    def test_tampered_upper_tree_signature_fails(self):
+        self.system_controller.queue_message(b"secure_message")
+        msg, sig = self.system_controller.sign_pending_batch()[0]
+
+        original_root = sig.upper_tree_signature.bottom_root
+        sig.upper_tree_signature.bottom_root = b"tampered_root_data123"
+        
+        self.assertFalse(self.system_controller.verify_message(msg, sig))
+        sig.upper_tree_signature.bottom_root = original_root
+        
+    def test_tampered_message_fails_bottom_tree(self):
+        self.system_controller.queue_message(b"original_message")
+        msg, sig = self.system_controller.sign_pending_batch()[0]
+
+        self.assertFalse(self.system_controller.verify_message(b"altered_message", sig))
 if __name__ == "__main__":
     unittest.main()
